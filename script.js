a11377095889f51dea9ac3d9dbea5733f6816a17
Dennis Best <dennis.best@me.com>
2024-04-25 00:09:37 -0700

    testing

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-// Create the observer like the examples above
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('section-animation');
-      return;
-    }
+document.addEventListener("DOMContentLoaded", function (event) {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("section-animation");
+        return;
+      }
 
-    entry.target.classList.remove('section-animation');
+      entry.target.classList.remove("section-animation");
+    });
   });
+
+  const squares = document.querySelectorAll(".animated-section");
+
+  squares.forEach((element) => observer.observe(element));
 });
-
-// Get multiple elements instead of a single one using "querySelectorAll"
-const squares = document.querySelectorAll('.animated-section');
-
-// Loop over the elements and add each one to the observer
-squares.forEach((element) => observer.observe(element));
