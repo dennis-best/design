de5d9d13d584b40f3cea6cd271e8d09eaa7ccc49
Dennis Best <dennis.best@me.com>
2024-04-25 07:29:01 -0700

    First major commit

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  fetch("nav.html")
+  fetch("footer.html")
     .then((res) => res.text())
     .then((text) => {
-      let oldelem = document.querySelector("script#replace_with_navbar");
+      let oldelem = document.querySelector("script#replace_with_footer");
       let newelem = document.createElement("div");
       newelem.innerHTML = text;
       oldelem.parentNode.replaceChild(newelem, oldelem);
